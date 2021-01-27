b501222fe93a710fedbe60030cf41112cc042a9d
kwoo <kwoo@ccli.com>
2021-01-27 11:20:54 -0800

    Initial files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Initialization
+
+```bash
+npm init @vitejs/app . --template vue-ts
+npm i 
+npm install -D tailwindcss@latest postcss@latest autoprefixer@latest
+npx tailwindcss init -p
+```
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+// TypeScript error? Run VSCode command
+// TypeScript: Select TypeScript version - > Use Workspace Version
+import App from './App.vue'
+
+import './styles/index.css'
+
+createApp(App).mount('#app')
